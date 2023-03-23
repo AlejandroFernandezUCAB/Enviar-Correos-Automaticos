817eadfad8a632cf124f245c7a290f0b90db4f6f
Pedro Fernandez <pedropaff7@gmail.com>
2023-03-22 20:46:33 -0400

    Agregar el envio de correo y de ping

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const nodemailer = require('nodemailer');
 const ping = require('net-ping');
 const dns = require('dns');
 var exec = require('child_process').exec;
-// website URL
+const fs = require('fs');
 
 require('dotenv').config();
 
 const url = 'google.com';
 
+// var minutes = 1,
+// 	the_interval = minutes * 60 * 1000;
+// setInterval(function () {
+// 	console.log('I am doing my' + minutes + 'minutes check');
+// 	createping(url);
+// }, the_interval);
 createping(url);
-
 async function createping(ip) {
-	exec('timeout 2 ping -t ' + ip, function (err, stdout, stderr) {
+	let date = new Date();
+
+	exec('timeout 1 ping -t ' + ip, function (err, stdout, stderr) {
 		//Funcion que envia correo electronico
 		var transporter = nodemailer.createTransport({
 			host: 'smtp-mail.outlook.com', // hostname
@@ -33,10 +40,35 @@ async function createping(ip) {
 			to: toAddress, // list of receivers (who receives)
 			subject:
 				'Recepcion de ping a las: ' +
-				convertTZ(new Date(), 'America/Caracas'), // Tue Apr 20 2012 17:10:30 GMT+0700 (Western Indonesia Time) // Subject line
+				convertTZ(date, 'America/Caracas'), // Tue Apr 20 2012 17:10:30 GMT+0700 (Western Indonesia Time) // Subject line
 			text: 'Hello world ', // plaintext body
 			html: stdout, // html body
 		};
+
+		let dateString =
+			date.getDate() +
+			'-' +
+			date.getMonth() +
+			'-' +
+			date.getFullYear() +
+			'-' +
+			date.getHours() +
+			'.' +
+			date.getMinutes() +
+			'.' +
+			date.getSeconds();
+
+		console.log(dateString);
+		fs.writeFile(
+			__dirname + '/ping-files/ping-' + dateString + '.txt',
+			date + ' ' + stdout,
+			function (err) {
+				if (err) {
+					return console.log(err);
+				}
+				console.log('The file was saved!');
+			}
+		);
 
 		transporter.sendMail(mailOptions, function (error, info) {
 			if (error) {
